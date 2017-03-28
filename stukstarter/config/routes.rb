bae0f0ee65c2f26b3905efb59f12f11fb5858fba
ajdin <ajdika@live.com>
2017-03-28 12:26:19 +0000

    Build New Reward form

--- stukstarter/config/routes.rb
+++ stukstarter/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'projects#index'
   devise_for :users
-  resources :projects
+  resources :projects do
+    resource :rewards, only: [:new, :create, :edit, :update, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
 
-  
 end
